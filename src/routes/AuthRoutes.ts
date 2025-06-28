c7a23eee314109333d19096b6890457a75489933
athulsabu <athulsabu75@gmail.com>
2025-06-28 11:55:02 +0530

    update: refresh token route added

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -6,6 +6,9 @@ const router = Router();
 const authController = new AuthController()
 
 router.post('/signUp', authController.createUser.bind(authController));
+
 router.post('/signIn', authController.signIn.bind(authController));
+
+router.post('/refresh', authController.refreshToken.bind(authController));
 
 export default router;
